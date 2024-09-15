d0a659a2ab8133fe68aa0a6a940fde4db2c4318b
Alvaro Augusto Perdigao Candido Fernandes <fernandes.alvaro@gmail.com>
2024-09-15 10:16:28 +1000

    login and logout completed

---
+++ routes.ts
@@ -0,0 +1,11 @@
+//Array of public routes @type {string[]}
+export const publicRoutes = ["/"];
+
+//Array of routes used for authentication @type {string[]}
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+//Prefix for api authentication @type {string}
+export const apiAuthPrefix = "/api/auth";
+
+//Default redirect @type {string}
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
